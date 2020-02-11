8c7ff1cb2bfefeeac9c35849553b9e65d89e2cb2
Kyungyoung Heo <bbvch13531@gmail.com>
2020-02-11 18:24:05 +0900

    Initial commit
    Signed-off-by: Kyungyoung Heo <bbvch13531@gmail.com>

---
+++ current-dir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "$(dirname "$0")"
+
+# echo current directory where shell script located
+# $ pwd
+# /home/ky
+# $ ~/Projects/shell-script/current-dir.sh
+# /home/ky/Projects/shell-script/
